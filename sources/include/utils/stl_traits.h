04e423ad0afb15d7fd8357aa6ad76ef34ca7af1f
rokoDev <mishaDoter@gmail.com>
2024-06-11 15:34:50 +0300

    use checkers for begin/end created by macros and added checkers for associative containers from std

--- sources/include/utils/stl_traits.h
+++ sources/include/utils/stl_traits.h
@@ -1,193 +1,104 @@
 #ifndef utils_stl_traits_h
 #define utils_stl_traits_h
 
+#include <map>
 #include <type_traits>
+#include <unordered_map>
 #include <utility>
+
+#include "function_traits.h"
 
 namespace utils
 {
-namespace details
+CREATE_R_METHOD_CHECKERS(size)
+
+CREATE_MEMBER_TYPE_CHECKERS(iterator)
+CREATE_METHOD_CHECKERS(begin)
+CREATE_METHOD_CHECKERS(end)
+
+CREATE_MEMBER_TYPE_CHECKERS(const_iterator)
+CREATE_METHOD_CHECKERS(cbegin)
+CREATE_METHOD_CHECKERS(cend)
+
+CREATE_FREE_FUNCTION_CHECKERS(begin)
+CREATE_FREE_FUNCTION_CHECKERS(end)
+
+CREATE_FREE_FUNCTION_CHECKERS(cbegin)
+CREATE_FREE_FUNCTION_CHECKERS(cend)
+
+template <typename T>
+struct is_std_map : std::false_type
 {
-template <typename T>
-using has_size_impl = decltype(std::declval<T>().size());
+};
 
-template <typename T>
-using has_begin_mem_impl = decltype(std::declval<T>().begin());
-
-template <typename T>
-using has_end_mem_impl = decltype(std::declval<T>().end());
-
-template <typename T>
-using has_cbegin_mem_impl = decltype(std::declval<T>().cbegin());
-
-template <typename T>
-using has_cend_mem_impl = decltype(std::declval<T>().cend());
-
-template <typename T>
-using has_begin_non_mem_impl = decltype(begin(std::declval<T>()));
-
-template <typename T>
-using has_end_non_mem_impl = decltype(end(std::declval<T>()));
-
-template <typename T>
-using has_cbegin_non_mem_impl = decltype(cbegin(std::declval<T>()));
-
-template <typename T>
-using has_cend_non_mem_impl = decltype(cend(std::declval<T>()));
-
-template <typename T, bool HasIterator>
-struct has_begin_mem : std::false_type
+template <class Key, class T, class Compare, class Allocator>
+struct is_std_map<std::map<Key, T, Compare, Allocator>> : std::true_type
 {
 };
 
 template <typename T>
-struct has_begin_mem<T, true>
-    : is_detected_exact<typename T::iterator, has_begin_mem_impl, T>
-{
-};
-
-template <typename T, bool HasIterator>
-struct has_end_mem : std::false_type
-{
-};
+inline constexpr bool is_std_map_v = is_std_map<T>::value;
 
 template <typename T>
-struct has_end_mem<T, true>
-    : is_detected_exact<typename T::iterator, has_end_mem_impl, T>
+struct is_std_multimap : std::false_type
 {
 };
 
-template <typename T, bool HasConstIterator>
-struct has_cbegin_mem : std::false_type
+template <class Key, class T, class Compare, class Allocator>
+struct is_std_multimap<std::multimap<Key, T, Compare, Allocator>>
+    : std::true_type
 {
 };
 
 template <typename T>
-struct has_cbegin_mem<T, true>
-    : is_detected_exact<typename T::const_iterator, has_cbegin_mem_impl, T>
-{
-};
-
-template <typename T, bool HasConstIterator>
-struct has_cend_mem : std::false_type
-{
-};
+inline constexpr bool is_std_multimap_v = is_std_multimap<T>::value;
 
 template <typename T>
-struct has_cend_mem<T, true>
-    : is_detected_exact<typename T::const_iterator, has_cend_mem_impl, T>
+struct is_std_unordered_map : std::false_type
 {
 };
-}  // namespace details
 
-template <typename T>
-struct has_size
-    : utils::is_detected_exact<std::size_t, details::has_size_impl, T>
+template <class Key, class T, class Hash, class KeyEqual, class Allocator>
+struct is_std_unordered_map<
+    std::unordered_map<Key, T, Hash, KeyEqual, Allocator>> : std::true_type
 {
 };
 
 template <typename T>
-inline constexpr bool has_size_v = has_size<T>::value;
+inline constexpr bool is_std_unordered_map_v = is_std_unordered_map<T>::value;
 
 template <typename T>
-struct has_iterator
+struct is_std_unordered_multimap : std::false_type
 {
-    template <typename U>
-    static std::true_type test(typename U::iterator *);
-
-    template <typename U>
-    static std::false_type test(...);
-
-    static constexpr bool value = decltype(test<T>(nullptr))::value;
 };
 
-template <typename T>
-inline constexpr bool has_iterator_v = has_iterator<T>::value;
-
-template <typename T>
-struct has_const_iterator
-{
-    template <typename U>
-    static std::true_type test(typename U::const_iterator *);
-
-    template <typename U>
-    static std::false_type test(...);
-
-    static constexpr bool value = decltype(test<T>(nullptr))::value;
-};
-
-template <typename T>
-inline constexpr bool has_const_iterator_v = has_const_iterator<T>::value;
-
-template <typename T>
-struct has_begin_mem : details::has_begin_mem<T, has_iterator_v<T>>
+template <class Key, class T, class Hash, class KeyEqual, class Allocator>
+struct is_std_unordered_multimap<
+    std::unordered_multimap<Key, T, Hash, KeyEqual, Allocator>> : std::true_type
 {
 };
 
 template <typename T>
-inline constexpr bool has_begin_mem_v = has_begin_mem<T>::value;
+inline constexpr bool is_std_unordered_multimap_v =
+    is_std_unordered_multimap<T>::value;
 
 template <typename T>
-struct has_end_mem : details::has_end_mem<T, has_iterator_v<T>>
+struct is_associative_std_container
+    : std::disjunction<is_std_map<T>, is_std_multimap<T>,
+                       is_std_unordered_map<T>, is_std_unordered_multimap<T>>
 {
 };
 
 template <typename T>
-inline constexpr bool has_end_mem_v = has_end_mem<T>::value;
-
-template <typename T>
-struct has_cbegin_mem : details::has_cbegin_mem<T, has_const_iterator_v<T>>
-{
-};
-
-template <typename T>
-inline constexpr bool has_cbegin_mem_v = has_cbegin_mem<T>::value;
-
-template <typename T>
-struct has_cend_mem : details::has_cend_mem<T, has_const_iterator_v<T>>
-{
-};
-
-template <typename T>
-inline constexpr bool has_cend_mem_v = has_cend_mem<T>::value;
-
-template <typename T>
-struct has_begin_non_mem : is_detected<details::has_begin_non_mem_impl, T>
-{
-};
-
-template <typename T>
-inline constexpr bool has_begin_non_mem_v = has_begin_non_mem<T>::value;
-
-template <typename T>
-struct has_end_non_mem : is_detected<details::has_end_non_mem_impl, T>
-{
-};
-
-template <typename T>
-inline constexpr bool has_end_non_mem_v = has_end_non_mem<T>::value;
-
-template <typename T>
-struct has_cbegin_non_mem : is_detected<details::has_cbegin_non_mem_impl, T>
-{
-};
-
-template <typename T>
-inline constexpr bool has_cbegin_non_mem_v = has_cbegin_non_mem<T>::value;
-
-template <typename T>
-struct has_cend_non_mem : is_detected<details::has_cend_non_mem_impl, T>
-{
-};
-
-template <typename T>
-inline constexpr bool has_cend_non_mem_v = has_cend_non_mem<T>::value;
+inline constexpr bool is_associative_std_container_v =
+    is_associative_std_container<T>::value;
 
 template <typename T>
 struct is_iterable
     : std::disjunction<
-          std::conjunction<has_begin_mem<T>, has_end_mem<T>>,
-          std::conjunction<has_begin_non_mem<T>, has_end_non_mem<T>>>
+          std::conjunction<has_type_iterator<T>, has_invocable_begin<T>,
+                           has_invocable_end<T>>,
+          std::conjunction<is_begin_invocable<T>, is_end_invocable<T>>>
 {
 };
 
@@ -197,8 +108,9 @@ inline constexpr bool is_iterable_v = is_iterable<T>::value;
 template <typename T>
 struct is_const_iterable
     : std::disjunction<
-          std::conjunction<has_cbegin_mem<T>, has_cend_mem<T>>,
-          std::conjunction<has_cbegin_non_mem<T>, has_cend_non_mem<T>>>
+          std::conjunction<has_type_const_iterator<T>, has_invocable_cbegin<T>,
+                           has_invocable_cend<T>>,
+          std::conjunction<is_cbegin_invocable<T>, is_cend_invocable<T>>>
 {
 };
 
@@ -207,7 +119,10 @@ inline constexpr bool is_const_iterable_v = is_const_iterable<T>::value;
 
 template <typename T>
 struct has_size_and_iterable_via_begin_end
-    : std::conjunction<has_size<T>, is_iterable<T>>
+    : std::conjunction<
+          std::disjunction<has_invocable_r_size_const_noexcept<std::size_t, T>,
+                           has_invocable_r_size_const<std::size_t, T>>,
+          is_iterable<T>>
 {
 };
 
--- tests/src/utils_stl_traits_tests.cpp
+++ tests/src/utils_stl_traits_tests.cpp
@@ -11,116 +11,134 @@
 
 TEST(StlTraits, HasSize)
 {
-    static_assert(utils::has_size_v<std::vector<char>>);
-    static_assert(utils::has_size_v<std::set<float>>);
-    static_assert(utils::has_size_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_size_v<std::string>);
-    static_assert(utils::has_size_v<std::valarray<int>>);
-    static_assert(utils::has_size_v<std::array<int, 10>>);
-    static_assert(not utils::has_size_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_size_v<int>);
+    static_assert(utils::has_invocable_r_size_const_noexcept_v<
+                  std::vector<char>::size_type, std::vector<char>>);
+    static_assert(
+        utils::has_invocable_r_size_const_noexcept_v<std::set<float>::size_type,
+                                                     std::set<float>>);
+    static_assert(utils::has_invocable_r_size_const_noexcept_v<
+                  std::unordered_map<int, std::string>::size_type,
+                  std::unordered_map<int, std::string>>);
+    static_assert(
+        utils::has_invocable_r_size_const_noexcept_v<std::string::size_type,
+                                                     std::string>);
+    static_assert(
+        utils::has_invocable_r_size_const_v<std::size_t, std::valarray<int>>);
+    static_assert(utils::has_invocable_r_size_const_noexcept_v<
+                  std::array<int, 10>::size_type, std::array<int, 10>>);
+    static_assert(not utils::has_invocable_r_size_const_noexcept_v<
+                  std::size_t, std::tuple<int, char, float>>);
+    static_assert(
+        not utils::has_invocable_r_size_const_noexcept_v<std::size_t, int>);
 }
 
 TEST(StlTraits, HasBeginMem)
 {
-    static_assert(utils::has_begin_mem_v<std::vector<char>>);
-    static_assert(utils::has_begin_mem_v<std::set<float>>);
-    static_assert(utils::has_begin_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_begin_mem_v<std::string>);
-    static_assert(not utils::has_begin_mem_v<std::valarray<int>>);
-    static_assert(utils::has_begin_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_begin_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_begin_mem_v<int>);
+    static_assert(utils::has_invocable_begin_v<std::vector<char>>);
+    static_assert(utils::has_invocable_begin_v<std::set<float>>);
+    static_assert(
+        utils::has_invocable_begin_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::has_invocable_begin_v<std::string>);
+    static_assert(not utils::has_invocable_begin_v<std::valarray<int>>);
+    static_assert(utils::has_invocable_begin_v<std::array<int, 10>>);
+    static_assert(
+        not utils::has_invocable_begin_v<std::tuple<int, char, float>>);
+    static_assert(not utils::has_invocable_begin_v<int>);
 }
 
 TEST(StlTraits, HasBeginNonMem)
 {
-    static_assert(utils::has_begin_non_mem_v<std::vector<char>>);
-    static_assert(utils::has_begin_non_mem_v<std::set<float>>);
+    static_assert(utils::is_begin_invocable_v<std::vector<char>>);
+    static_assert(utils::is_begin_invocable_v<std::set<float>>);
     static_assert(
-        utils::has_begin_non_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_begin_non_mem_v<std::string>);
-    static_assert(utils::has_begin_non_mem_v<std::valarray<int>>);
-    static_assert(utils::has_begin_non_mem_v<const std::valarray<int>>);
-    static_assert(utils::has_begin_non_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_begin_non_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_begin_non_mem_v<int>);
+        utils::is_begin_invocable_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::is_begin_invocable_v<std::string>);
+    static_assert(utils::is_begin_invocable_v<std::valarray<int>>);
+    static_assert(utils::is_begin_invocable_v<const std::valarray<int>>);
+    static_assert(utils::is_begin_invocable_v<std::array<int, 10>>);
+    static_assert(
+        not utils::is_begin_invocable_v<std::tuple<int, char, float>>);
+    static_assert(not utils::is_begin_invocable_v<int>);
 }
 
 TEST(StlTraits, HasEndMem)
 {
-    static_assert(utils::has_end_mem_v<std::vector<char>>);
-    static_assert(utils::has_end_mem_v<std::set<float>>);
-    static_assert(utils::has_end_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_end_mem_v<std::string>);
-    static_assert(not utils::has_end_mem_v<std::valarray<int>>);
-    static_assert(utils::has_end_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_end_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_end_mem_v<int>);
+    static_assert(utils::has_invocable_end_v<std::vector<char>>);
+    static_assert(utils::has_invocable_end_v<std::set<float>>);
+    static_assert(
+        utils::has_invocable_end_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::has_invocable_end_v<std::string>);
+    static_assert(not utils::has_invocable_end_v<std::valarray<int>>);
+    static_assert(utils::has_invocable_end_v<std::array<int, 10>>);
+    static_assert(not utils::has_invocable_end_v<std::tuple<int, char, float>>);
+    static_assert(not utils::has_invocable_end_v<int>);
 }
 
 TEST(StlTraits, HasEndNonMem)
 {
-    static_assert(utils::has_end_non_mem_v<std::vector<char>>);
-    static_assert(utils::has_end_non_mem_v<std::set<float>>);
+    static_assert(utils::is_end_invocable_v<std::vector<char>>);
+    static_assert(utils::is_end_invocable_v<std::set<float>>);
     static_assert(
-        utils::has_end_non_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_end_non_mem_v<std::string>);
-    static_assert(utils::has_end_non_mem_v<std::valarray<int>>);
-    static_assert(utils::has_end_non_mem_v<const std::valarray<int>>);
-    static_assert(utils::has_end_non_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_end_non_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_end_non_mem_v<int>);
+        utils::is_end_invocable_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::is_end_invocable_v<std::string>);
+    static_assert(utils::is_end_invocable_v<std::valarray<int>>);
+    static_assert(utils::is_end_invocable_v<const std::valarray<int>>);
+    static_assert(utils::is_end_invocable_v<std::array<int, 10>>);
+    static_assert(not utils::is_end_invocable_v<std::tuple<int, char, float>>);
+    static_assert(not utils::is_end_invocable_v<int>);
 }
 
 TEST(StlTraits, HasConstBeginMem)
 {
-    static_assert(utils::has_cbegin_mem_v<std::vector<char>>);
-    static_assert(utils::has_cbegin_mem_v<std::set<float>>);
+    static_assert(utils::has_invocable_cbegin_v<std::vector<char>>);
+    static_assert(utils::has_invocable_cbegin_v<std::set<float>>);
     static_assert(
-        utils::has_cbegin_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_cbegin_mem_v<std::string>);
-    static_assert(not utils::has_cbegin_mem_v<std::valarray<int>>);
-    static_assert(utils::has_cbegin_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_cbegin_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_cbegin_mem_v<int>);
+        utils::has_invocable_cbegin_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::has_invocable_cbegin_v<std::string>);
+    static_assert(not utils::has_invocable_cbegin_v<std::valarray<int>>);
+    static_assert(utils::has_invocable_cbegin_v<std::array<int, 10>>);
+    static_assert(
+        not utils::has_invocable_cbegin_v<std::tuple<int, char, float>>);
+    static_assert(not utils::has_invocable_cbegin_v<int>);
 }
 
 TEST(StlTraits, HasConstBeginNonMem)
 {
-    static_assert(utils::has_cbegin_non_mem_v<std::vector<char>>);
-    static_assert(utils::has_cbegin_non_mem_v<std::set<float>>);
+    static_assert(utils::is_cbegin_invocable_v<std::vector<char>>);
+    static_assert(utils::is_cbegin_invocable_v<std::set<float>>);
     static_assert(
-        utils::has_cbegin_non_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_cbegin_non_mem_v<std::string>);
-    static_assert(utils::has_cbegin_non_mem_v<std::array<int, 10>>);
+        utils::is_cbegin_invocable_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::is_cbegin_invocable_v<std::string>);
+    static_assert(utils::is_cbegin_invocable_v<std::array<int, 10>>);
     static_assert(
-        not utils::has_cbegin_non_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_cbegin_non_mem_v<int>);
+        not utils::is_cbegin_invocable_v<std::tuple<int, char, float>>);
+    static_assert(not utils::is_cbegin_invocable_v<int>);
 }
 
 TEST(StlTraits, HasConstEndMem)
 {
-    static_assert(utils::has_cend_mem_v<std::vector<char>>);
-    static_assert(utils::has_cend_mem_v<std::set<float>>);
-    static_assert(utils::has_cend_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_cend_mem_v<std::string>);
-    static_assert(not utils::has_cend_mem_v<std::valarray<int>>);
-    static_assert(utils::has_cend_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_cend_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_cend_mem_v<int>);
+    static_assert(utils::has_invocable_cend_v<std::vector<char>>);
+    static_assert(utils::has_invocable_cend_v<std::set<float>>);
+    static_assert(
+        utils::has_invocable_cend_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::has_invocable_cend_v<std::string>);
+    static_assert(not utils::has_invocable_cend_v<std::valarray<int>>);
+    static_assert(utils::has_invocable_cend_v<std::array<int, 10>>);
+    static_assert(
+        not utils::has_invocable_cend_v<std::tuple<int, char, float>>);
+    static_assert(not utils::has_invocable_cend_v<int>);
 }
 
 TEST(StlTraits, HasConstEndNonMem)
 {
-    static_assert(utils::has_cend_non_mem_v<std::vector<char>>);
-    static_assert(utils::has_cend_non_mem_v<std::set<float>>);
+    static_assert(utils::is_cend_invocable_v<std::vector<char>>);
+    static_assert(utils::is_cend_invocable_v<std::set<float>>);
     static_assert(
-        utils::has_cend_non_mem_v<std::unordered_map<int, std::string>>);
-    static_assert(utils::has_cend_non_mem_v<std::string>);
-    static_assert(utils::has_cend_non_mem_v<std::array<int, 10>>);
-    static_assert(not utils::has_cend_non_mem_v<std::tuple<int, char, float>>);
-    static_assert(not utils::has_cend_non_mem_v<int>);
+        utils::is_cend_invocable_v<std::unordered_map<int, std::string>>);
+    static_assert(utils::is_cend_invocable_v<std::string>);
+    static_assert(utils::is_cend_invocable_v<std::array<int, 10>>);
+    static_assert(not utils::is_cend_invocable_v<std::tuple<int, char, float>>);
+    static_assert(not utils::is_cend_invocable_v<int>);
 }
 
 TEST(StlTraits, IsIterable)
